8f44cadbd95db7d2cfbc46c62b3a2757f539cb13
bachaPro <zhouyaofei@gmail.com>
2014-08-24 23:57:26 -0400

    test for dao

--- src/test/java/com/nash/test/dao/ScheduleDaoTest.java
+++ src/test/java/com/nash/test/dao/ScheduleDaoTest.java
@@ -1,12 +1,18 @@
 package com.nash.test.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.nash.dao.LocationDao;
+import com.nash.model.Location;
 
-public class ScheduleDaoTest  {
 
- public void testSave(){
-	 
- }
+
+public class ScheduleDaoTest extends BaseDaoTest {
  
+
+ LocationDao dao;
+ 
+
 }
